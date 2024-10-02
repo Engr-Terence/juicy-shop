b9fe615f1a8e7d761d7234a9d0ad49fd68264a16
Engr-Terence <terenceaatarah@gmail.com>
2024-10-02 10:33:26 -0400

    Uploading result to Defectdejo

---
+++ upload-results.py
@@ -0,0 +1,40 @@
+import requests
+import sys
+
+file_name = sys.argv[1]
+scan_type = ''
+
+if file_name == 'gitleaks.json':
+    scan_type = 'Gitleaks Scan'
+elif file_name == 'njsscan.sarif':
+    scan_type = 'SARIF'
+elif file_name == 'semgrep.json':
+    scan_type = 'Semgrep JSON Report'
+elif file_name == 'retire.json':
+    scan_type = 'Retire.js Scan'
+
+
+headers = {
+    'Authorization': 'Token 4ae385495af99fe3b5b02bf0baec4f7b89f6683d'
+}
+
+url = 'https://demo.defectdojo.org/api/v2/import-scan/'
+
+data = {
+    'active': True,
+    'verified': True,
+    'scan_type': scan_type,
+    'minimum_severity': 'Low',
+    'engagement': 20
+}
+
+files = {
+    'file': open(file_name, 'rb')
+}
+
+response = requests.post(url, headers=headers, data=data, files=files)
+
+if response.status_code == 201:
+    print('Scan results imported successfully')
+else:
+    print(f'Failed to import scan results: {response.content}')
